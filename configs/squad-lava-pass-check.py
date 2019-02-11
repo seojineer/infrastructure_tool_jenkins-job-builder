62cc4f6c7a1a9517972e9547dd44f444de671bb4
suker <suker@nexell.co.kr>
2019-02-11 13:51:41 +0900

    ci/cd: infrastructure_tool_jenkins-job-builder: reporting script and reference update
    This patch update for show link about LAVA testjob result when test failure with
    reference test yaml files.
    
    Signed-off-by: suker <suker@nexell.co.kr>
    Change-Id: Ice9f49859914299cda6f32bcbc837a613cc0bdfb
    Reviewed-on: https://review.gerrithub.io/c/444021
    Tested-by: Nexell Jenkins <jenkins@nexell.co.kr>

--- configs/squad-lava-pass-check.py
+++ configs/squad-lava-pass-check.py
@@ -1,6 +1,7 @@
 import pycurl
 from StringIO import StringIO
 import sys
+from squad_lava_report_detail import rdmain
 
 pass_condition = {'"job_status"': '"Complete"', '"failure"': "null"}
 
@@ -24,7 +25,7 @@ def resultParse(url):
             # print(_.split(':')[1])
             is_job_status_complete = True
         elif pass_condition.keys()[1] in _ and \
-           _.split(':')[1] == pass_condition.values()[1]:  # failure is Null
+             _.split(':')[1] == pass_condition.values()[1]:  # failure is Null
             # print(_.split(':')[1])
             is_failure_numm = True
         else:
@@ -39,6 +40,8 @@ def resultParse(url):
 def main(arg1):
     ret = resultParse(arg1)
     print(ret)
+    if ret == "LAVA Test Fail!" :
+        rdmain(arg1)
     return ret
 
 
---
+++ configs/squad_lava_report_detail.py
@@ -0,0 +1,30 @@
+import pycurl
+from StringIO import StringIO
+
+reportList = ['"failure"', '"external_url"']
+
+
+def resultParse(url):
+    buffer = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+
+    body = buffer.getvalue()
+    tmpHash = body[1:-1].split(',')
+
+    for _ in tmpHash:
+        if reportList[0] in _ :
+            print(_.split(':')[2])
+        elif reportList[1] in _ :
+            print("http" + _.split('http')[1][:-1])
+        else:
+            pass
+
+
+def rdmain(arg1):
+    ret = resultParse(arg1)
+    print(ret)
+    return ret
