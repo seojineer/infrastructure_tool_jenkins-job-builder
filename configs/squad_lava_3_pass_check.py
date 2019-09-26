dfaeb55a500153014fb141d90f1381cbc0304c21
seoji <seoji@nexell.co.kr>
2019-09-26 10:21:07 +0900

    ci/cd: infrastructure_tool_jenkins-job-builder: update squad testjobs report
    This patch updates Squad's pass/fail parsing scheme for duplicated
    job_id issue.
    
    Signed-off-by: seoji <seoji@nexell.co.kr>
    Change-Id: I894d785f89b5508694cadf9a19e7d5873687c495
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/infrastructure_tool_jenkins-job-builder/+/469494
    Tested-by: Nexell Jenkins <jenkins@nexell.co.kr>
    Tested-by: Sungwoo Park <swpark@nexell.co.kr>
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>

--- configs/squad_lava_3_pass_check.py
+++ configs/squad_lava_3_pass_check.py
@@ -2,7 +2,7 @@ import pycurl
 from StringIO import StringIO
 from squad_lava_4_report_detail import report_detail_main
 
-pass_condition = {'"job_status"': '"Complete"', '"failure"': "null"}
+pass_condition = {'"job_status"': '"Complete"', '"failure"': '"null"'}
 FAIL_MARK = "LAVA Test Fail!"
 PASS_MARK = "LAVA Test Complete"
 
@@ -19,19 +19,28 @@ def resultParse(url):
     tmpHash = body[1:-1].split(',')
     is_job_status_complete = False
     is_failure_null = False
+    is_failure_exception = False
 
     for _ in tmpHash:
         if pass_condition.keys()[0] in _ and _.split(':')[1] == pass_condition.values()[0]:  # job_status is Complete
             # print(_.split(':')[1])
             is_job_status_complete = True
-        elif pass_condition.keys()[1] in _ and _.split(':')[1] == pass_condition.values()[1]:  # failure is Null
-            # print(_.split(':')[1])
-            is_failure_null = True
+        #elif pass_condition.keys()[1] in _ and _.split(':')[1] == pass_condition.values()[1]:  # failure is Null
+        elif pass_condition.keys()[1] in _:
+            if _.split(':')[1] == pass_condition.values()[1]:  # failure is Null
+                print(_.split(':')[1])
+                is_failure_null = True
+            else: # 'There is already a test run with job_id' excption occurs (bug)
+                is_failure_exception = True
+                pass
         else:
             pass
 
-    if is_job_status_complete and is_failure_null :
-        return PASS_MARK
+    if is_job_status_complete:
+        if is_failure_null:
+            return PASS_MARK
+        elif is_failure_exception: # for pass job_id corruption bug
+            return PASS_MARK
     else:
         return FAIL_MARK
 
